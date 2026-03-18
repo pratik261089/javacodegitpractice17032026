b39033cbe46b4598fc3cadf72981ef7b365af18a
pratik bhosle <pratikbhosle.21@gmail.com>
2026-03-17 17:06:40 -1200

    Test03 class added

---
+++ TestGit/com/pratice/Test03.java
@@ -0,0 +1,15 @@
+package pratice;
+
+public class Test03 {
+
+	public static void main(String[] args) {
+		
+		
+		System.out.println("Test03");
+		
+		System.out.println("New line added");
+		
+
+	}
+
+}
