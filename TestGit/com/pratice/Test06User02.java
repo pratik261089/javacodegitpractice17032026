9c6f0e429458c12353ab6444ce70fbb622411df0
pratik bhosle <pratikbhosle.21@gmail.com>
2026-03-18 08:02:38 -1200

    Test 06 and Test 07 added by User02

---
+++ TestGit/com/pratice/Test06User02.java
@@ -0,0 +1,12 @@
+package pratice;
+
+public class Test06User02 {
+
+	public static void main(String[] args) {
+		
+		
+		System.out.println("Test06 added by user02");
+
+	}
+
+}
---
+++ TestGit/com/pratice/Test07User02.java
@@ -0,0 +1,12 @@
+package pratice;
+
+public class Test07User02 {
+
+	public static void main(String[] args) {
+		
+		
+		System.out.println("Test07");
+
+	}
+
+}
