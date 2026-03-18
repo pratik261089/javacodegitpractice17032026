1a4c71db3195bd4ce9bb5c6c35f2bce4f4dfeee3
pratik bhosle <pratikbhosle.21@gmail.com>
2026-03-17 17:17:48 -1200

    Test04 and Test05 added in featues branch

---
+++ TestGit/com/pratice/Test04.java
@@ -0,0 +1,10 @@
+package pratice;
+
+public class Test04 {
+
+	public static void main(String[] args) {
+		
+		System.out.println("Test04");
+	}
+
+}
---
+++ TestGit/com/pratice/Test05.java
@@ -0,0 +1,12 @@
+package pratice;
+
+public class Test05 {
+
+	public static void main(String[] args) {
+		
+		
+		System.out.println("Test05");
+
+	}
+
+}
